46f262abc818af485b08a509d2eb12f5d505cf08
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-27 18:08:10 +0700

    add section moblie top sell

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ $(document).ready(function(){
     // banner owl carousel
     $("#banner-area .owl-carousel").owlCarousel({
         dots: true,
-        items: 1
+        items: 1,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        loop: true
     });
+    // Mobile top salse
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots: false,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items:5
+            }
+        }
+    })
 });
