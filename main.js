b5fa121a5dc510a160c59060b5783afcf838b69f
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-09-27 20:24:02 +0700

    use library isotope to filter type mobile

--- main.js
+++ main.js
@@ -24,4 +24,19 @@ $(document).ready(function(){
             }
         }
     })
+
+    // use isotope filter 
+
+        var $grid = $(".grid").isotope({
+            itemSelector: '.grid-item',
+            layoutMode: 'fitRows'
+        });
+
+        // filter items on button click
+        $(".button-group").on("click", "button", function(){
+            var filterValue = $(this).attr('data-filter');
+            $grid.isotope({filter:filterValue});
+        })
+
+
 });
